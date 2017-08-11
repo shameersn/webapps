09327b9744e936405fa841176484608d12483744
ShameerSN <shameersalnaz@gmail.com>
2017-08-11 06:17:10 +0530

    Init, Client with react, simple express server

---
+++ simple-cart/client/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookList from './BookList';
+
+class App extends React.Component{
+  render(){
+    return(
+      <BookList />
+    );
+  }
+}
+
+export default App;
---
+++ simple-cart/client/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Book from './Book';
+
+const BookList = () => {
+  return(
+    <div>
+      <Book />
+      <Book />
+      <Book />
+    </div>
+  )
+};
+
+export default BookList;
